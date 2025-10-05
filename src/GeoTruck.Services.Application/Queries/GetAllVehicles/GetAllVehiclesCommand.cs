752e3a00d937a5707bbd36c3b6ffa5abc6fbb9b2
Adriano Victor dos Santos <adriano.victor@xgen.com.br>
2025-10-04 22:39:28 -0300

    Troca de record por class para o ccommand.

--- src/GeoTruck.Services.Application/Queries/GetAllVehicles/GetAllVehiclesCommand.cs
+++ src/GeoTruck.Services.Application/Queries/GetAllVehicles/GetAllVehiclesCommand.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace GeoTruck.Services.Application.Queries.GetAllVehicles;
 
-public record GetAllVehiclesCommand(
+public class GetAllVehiclesCommand(
     string? Renavam,
     string? Plate,
     string? Model,
@@ -11,5 +11,14 @@ public record GetAllVehiclesCommand(
     int? Year,
     int Limit,
     int Offset
-) : IRequest<GetAllVehiclesResponse>;
+) : IRequest<GetAllVehiclesResponse>
+{
+    public string? Renavam { get; set; } = Renavam;
+    public string? Plate { get; set; } = Plate;
+    public string? Model { get; set; } = Model;
+    public string? Brand { get; set; } = Brand;
+    public int? Year { get; set; } = Year;
+    public int Limit { get; set; } = Limit;
+    public int Offset { get; set; } = Offset;
+}
 
